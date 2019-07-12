e11c6f32464672e2ac117aa93d7f88d626ad788b
jose frias <josefriasruiz@hotmail.com>
2019-07-12 17:16:29 -0600

    Se modifica el codigo

--- src/proyecto_final/Proyecto_Final.java
+++ src/proyecto_final/Proyecto_Final.java
@@ -16,6 +16,7 @@ public class Proyecto_Final {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("El marco se droga");
     }
     
 }
