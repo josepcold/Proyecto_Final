2f43e4d7cc59c7edf52fcc27dcef0633fc135893
SERGIO LOPEZ <SERGIO LOPEZ@DESKTOP-9CIM394>
2019-07-11 19:32:47 -0600

    Hola mundo

--- src/proyecto_final/Proyecto_Final.java
+++ src/proyecto_final/Proyecto_Final.java
@@ -16,6 +16,9 @@ public class Proyecto_Final {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hola mundo"
+                + "");
     }
     
 }
